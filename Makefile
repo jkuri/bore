1fb061f2a85041c3128b68e3d9aac7057c27ddd6
Jan Kuri <jkuri88@gmail.com>
2020-06-30 16:03:58 +0200

    feat(web): initial landing page, needs work

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-build: wire build_server build_client
+build: wire statik_landing build_server build_client
 
 build_server:
 	@CGO_ENABLED=0 go build -o ./build/bore-server ./cmd/bore-server
@@ -9,7 +9,10 @@ build_client:
 wire:
 	@wire ./cmd/bore-server
 
+statik_landing:
+	@if [ ! -r "internal/ui/landing/statik.go" ]; then statik -dest ./internal/ui -p landing -src ./web/bore-landing/dist; fi
+
 install_dependencies:
-	@go get github.com/google/wire/cmd/...
+	@go get github.com/jkuri/statik github.com/google/wire/cmd/...
 
 .PHONY: wire build_server build_client build
--- web/bore-landing/src/app/app.component.ts
+++ web/bore-landing/src/app/app.component.ts
@@ -1,32 +1,70 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { interval, of, Subscription } from 'rxjs';
+import { tap, takeWhile, delay } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
-  template: `
-    <!--The content below is only a placeholder and can be replaced.-->
-    <div style="text-align:center" class="content">
-      <h1>
-        Welcome to {{title}}!
-      </h1>
-      <span style="display: block">{{ title }} app is running!</span>
-      <img width="300" alt="Angular Logo" src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAyNTAgMjUwIj4KICAgIDxwYXRoIGZpbGw9IiNERDAwMzEiIGQ9Ik0xMjUgMzBMMzEuOSA2My4ybDE0LjIgMTIzLjFMMTI1IDIzMGw3OC45LTQzLjcgMTQuMi0xMjMuMXoiIC8+CiAgICA8cGF0aCBmaWxsPSIjQzMwMDJGIiBkPSJNMTI1IDMwdjIyLjItLjFWMjMwbDc4LjktNDMuNyAxNC4yLTEyMy4xTDEyNSAzMHoiIC8+CiAgICA8cGF0aCAgZmlsbD0iI0ZGRkZGRiIgZD0iTTEyNSA1Mi4xTDY2LjggMTgyLjZoMjEuN2wxMS43LTI5LjJoNDkuNGwxMS43IDI5LjJIMTgzTDEyNSA1Mi4xem0xNyA4My4zaC0zNGwxNy00MC45IDE3IDQwLjl6IiAvPgogIDwvc3ZnPg==">
-    </div>
-    <h2>Here are some links to help you start: </h2>
-    <ul>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://angular.io/tutorial">Tour of Heroes</a></h2>
-      </li>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://angular.io/cli">CLI Documentation</a></h2>
-      </li>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://blog.angular.io/">Angular blog</a></h2>
-      </li>
-    </ul>
-    
-  `,
-  styles: []
+  templateUrl: './app.component.html'
 })
-export class AppComponent {
-  title = 'bore-landing';
+export class AppComponent implements OnInit, OnDestroy {
+  command: string = 'bore -s bore.network -lp 8080';
+  result: string = 'Generated URL: https://532bbf43.bore.network';
+  displayCommand: string = '';
+  displayResult: string = '';
+  sub = new Subscription();
+
+  ngOnInit(): void {
+    let index = 0;
+    interval(100)
+      .pipe(
+        tap(() => index++),
+        takeWhile(() => index <= this.command.length)
+      )
+      .subscribe(
+        () => {
+          const cmd = this.command.slice(0);
+          this.displayCommand = cmd.slice(0, index);
+        },
+        err => {
+          console.error(err);
+        },
+        () => {
+          of(this.result)
+            .pipe(delay(500))
+            .subscribe(result => (this.displayResult = result));
+        }
+      );
+  }
+
+  ngOnDestroy(): void {
+    this.sub.unsubscribe();
+  }
+
+  type(): void {
+    let index = 0;
+
+    this.sub.add(
+      interval(80)
+        .pipe(
+          tap(() => index++),
+          takeWhile(() => index <= this.command.length)
+        )
+        .subscribe(
+          () => {
+            const cmd = this.command.slice(0);
+            this.displayCommand = cmd.slice(0, index);
+          },
+          err => {
+            console.error(err);
+          },
+          () => {
+            this.sub.add(
+              of(this.result)
+                .pipe(delay(500))
+                .subscribe(result => (this.displayResult = result))
+            );
+          }
+        )
+    );
+  }
 }
