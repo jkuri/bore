92498fe4025d9a7e4e1524df92cd5f4ea4ecb7f4
Jan Kuri <jkuri88@gmail.com>
2021-06-16 12:49:51 +0200

    chore(release): 0.4.0

--- internal/version/version.go
+++ internal/version/version.go
@@ -1,7 +1,7 @@
 package version
 
 // Version represents current version.
-const Version = "0.3.0"
+const Version = "0.4.0"
 
 var (
 	// UIVersion is build time var and represents version of the user interface
